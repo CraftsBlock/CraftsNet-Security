66e8ceb49ba00e3bc61507232c25728c742cef1e
Philipp <62473021+CrAfTsArMy@users.noreply.github.com>
2025-03-06 18:08:35 +0100

    [CH] Corrected the TokenUsedEvent

--- src/main/java/de/craftsblock/cnet/modules/security/events/auth/token/TokenUsedEvent.java
+++ src/main/java/de/craftsblock/cnet/modules/security/events/auth/token/TokenUsedEvent.java
@@ -1,6 +1,7 @@
 package de.craftsblock.cnet.modules.security.events.auth.token;
 
 import de.craftsblock.cnet.modules.security.auth.token.Token;
+import de.craftsblock.cnet.modules.security.auth.token.adapter.TokenAuthType;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -8,21 +9,35 @@ import org.jetbrains.annotations.NotNull;
  *
  * @author Philipp Maywald
  * @author CraftsBlock
- * @version 1.0.0
+ * @version 1.0.1
  * @see GenericTokenEvent
  * @see Token
  * @since 1.0.0-SNAPSHOT
  */
 public class TokenUsedEvent extends GenericTokenEvent {
 
+    private final TokenAuthType type;
+
     /**
      * Constructs a new {@link TokenUsedEvent}.
      *
-     * @param token The token that has been used. Must not be null.
+     * @param token The {@link Token} that has been used. Must not be null.
+     * @param type  The {@link TokenAuthType} where the {@link Token} was found. Must not be null.
      * @throws NullPointerException If {@code token} is null.
      */
-    public TokenUsedEvent(@NotNull Token token) {
+    public TokenUsedEvent(@NotNull Token token, @NotNull TokenAuthType type) {
         super(token);
+        this.type = type;
+    }
+
+    /**
+     * Retrieves the {@link TokenAuthType} where the {@link Token} was
+     * found.
+     *
+     * @return The {@link TokenAuthType} where the {@link Token} was found.
+     */
+    public @NotNull TokenAuthType getAuthType() {
+        return type;
     }
 
 }

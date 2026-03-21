630fe3f588506cf851713988c98587ef08e910a4
Philipp <62473021+CrAfTsArMy@users.noreply.github.com>
2026-03-21 11:43:42 +0100

    feat: Add no op sql reload provider

---
+++ sql/src/main/java/de/craftsblock/cnet/modules/security/token/driver/sql/reload/SQLNoOpReloadProvider.java
@@ -0,0 +1,12 @@
+package de.craftsblock.cnet.modules.security.token.driver.sql.reload;
+
+import de.craftsblock.cnet.modules.security.token.driver.sql.SQLStoreDriver;
+import org.jetbrains.annotations.NotNull;
+
+public class SQLNoOpReloadProvider extends SQLReloadProvider {
+
+    public SQLNoOpReloadProvider(@NotNull SQLStoreDriver driver) {
+        super(driver);
+    }
+
+}

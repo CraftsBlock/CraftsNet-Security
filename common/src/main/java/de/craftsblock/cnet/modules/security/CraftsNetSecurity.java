178a0bcc6cfc6146741b971628714cb1c1f6b8a9
Philipp <62473021+CrAfTsArMy@users.noreply.github.com>
2026-05-14 00:50:51 +0200

    chore: Bump version to 1.0.0-pre3

--- common/src/main/java/de/craftsblock/cnet/modules/security/CraftsNetSecurity.java
+++ common/src/main/java/de/craftsblock/cnet/modules/security/CraftsNetSecurity.java
@@ -30,7 +30,7 @@ public final class CraftsNetSecurity extends Addon {
     /**
      * The current version of the security module.
      */
-    public static final String VERSION = "1.0.0-pre2";
+    public static final String VERSION = "1.0.0-pre3";
 
     private AuthChain authChain;
 
